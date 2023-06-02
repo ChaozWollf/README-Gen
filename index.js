837dbc34773381015810f3fe9ed6eab9e008ed53
ChaozWollf <128013056+ChaozWollf@users.noreply.github.com>
2023-06-02 17:23:25 -0500

    added questions

--- index.js
+++ index.js
@@ -1,10 +1,80 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const fs = require("fs");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = []
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'what is the name of your project?'
+
+        },
+        {
+            type: 'checkbox',
+            name: 'options',
+            message: 'which of the following do you want to include?',
+            choices: ['Description', 'Table of Contents', 'Installation', 'Usage', 'License', 'Contrubuting', 'Tests', 'Questions']
+
+        },
+
+        {
+            type: 'input',
+            name: 'Description',
+            message: 'Description of Project',
+        },
+
+        {
+            type: 'input',
+            name: 'Installation',
+            message: 'Installation instructions'
+
+        },
+        {
+            type: 'input',
+            name: 'Usage',
+            message: 'How is this project to be used?'
+        },
+        {
+            type: 'checkbox',
+            name: 'License',
+            choices: ['MIT', 'None']
+        },
+        {
+            type: 'input',
+            name: 'Contriubting',
+            message: 'List your Contributors'
+        },
+        {
+            type: '',
+            name: 'Tests',
+            Message: 'what tests do you want?'
+        },
+
+        {
+            type: 'input',
+            name:'Github',
+            message: 'What is your Github username?(this will be used for questions people may have and to use some of your open source code',
+        },
+        {
+            type:'input',
+            name:'email',
+            message:'what is your email?(this will be used for questions people may have)'
+        }
+    ])
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data,(err) =>{
+        if (err) {
+            consol.error('Error writing file', err);
+        } else{
+            console.log ('Data written to file sucessfully')
+        }
+        });
+    }
+const fileName= `${data.name}README.md`
 
 // TODO: Create a function to initialize app
 function init() {}
