82d78eb22c3a1c492bfd2903bb8eb366ac6f25b4
ChaozWollf <128013056+ChaozWollf@users.noreply.github.com>
2023-06-10 11:07:54 -0500

    finnished all I could

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
-// GIVEN a command - line application that accepts user input      
+      
 // WHEN I am prompted for information about my application repository  
 // THEN a high - quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
-// WHEN I enter my project title
-// THEN this is displayed as the title of the README
-// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
-// WHEN I choose a license for my application from a list of options
-// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-// WHEN I enter my GitHub username
-// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
-// WHEN I enter my email address
-// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+
+
+
+
+
 // WHEN I click on the links in the Table of Contents
 // THEN I am taken to the corresponding section of the README
 //     ```
 
-// TODO: Include packages needed for this application
+
 const inquirer = require("inquirer");
 const fs = require("fs");
 const generateMarkdown = require("./utils/generateMarkdown");
@@ -24,8 +19,6 @@ function writeToFile(fileName, data) {
     return fs.writeFileSync(fileName, data)
 }
 
-// // TODO: Create an array of questions for user input
-// const questions = []
 inquirer
     .prompt([
         {
@@ -34,13 +27,14 @@ inquirer
             message: 'what is the name of your project?'
 
         },
-        {
-            type: 'checkbox',
-            name: 'options',
-            message: 'which of the following do you want to include?',
-            choices: ['Description', 'Table of Contents', 'Installation', 'Usage', 'License', 'Contrubuting', 'Tests', 'Questions']
+        // this section was commented out to remind me that this is something I want for future development
+        // {
+        //     type: 'checkbox',
+        //     name: 'options',
+        //     message: 'which of the following do you want to include?',
+        //     choices: ['Description', 'Table of Contents', 'Installation', 'Usage', 'License', 'Contrubuting', 'Tests', 'Questions']
 
-        },
+        // },
 
         {
             type: 'input',
@@ -88,10 +82,7 @@ inquirer
     ]).then((answers) => {
         writeToFile(`README.md`, generateMarkdown({ ...answers}));
         
-    //    fs.writeFile(`README.md`, template(answers),
-    //         // (err) =>err ? console.error(err):console.log('Sucess!')
-    //     );
-    });
+        });
     // fs.writeFile(`README.md`, template(answers), 
     // (err) =>err ? console.error(err):console.log('Sucess!')
     // );
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,29 +1,26 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+
 function renderLicenseBadge(license) {if ((license.license) == 'MIT'){return "![Github license](https://img.shields.io/badge/licence-MIT-red.svg)";
 } else {
-  return 'Empty string'   
+  return ''   
 };
 };
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
+
 function renderLicenseLink(license) {
   if (license == "MIT"){
     return "https://drewdevault.com/2022/02/07/Free-software-licenses-MIT.html"
   } else{
-    return 'Empty string';
+    return '';
   };
   
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
+
 function renderLicenseSection(license) {
  return renderLicenseBadge(license) +
   renderLicenseLink(license);
 }
 
-// TODO: Create a function to generate markdown for README
+
 function generateMarkdown(data) {
   return `# ${data.name}
     
@@ -47,21 +44,10 @@ function generateMarkdown(data) {
                      [${data.email}](mailto:${data.email})  `
  };
             
-        // module.exports = {
-        //     fileName,
-        //     data,
-        // };
-        // console.log(fileName);
-        // console.log(data);
-    
-
-    // template(answers);
+       
 
 
-;
 
 
 module.exports = generateMarkdown;
-
-// https://img.shields.io/badge/licence-MIT-red
 
